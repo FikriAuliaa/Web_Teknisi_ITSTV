b47c8f94faa23ebc841548f4c9c2961b7a340abe
FikriAuliaa <fikrisaad14@gmail.com>
2025-01-21 20:31:05 +0700

    SUmmaryPage

--- client/src/views/SummaryPage.vue
+++ client/src/views/SummaryPage.vue
@@ -2,12 +2,12 @@
   <div class="container mx-auto px-4 py-8">
     <h1 class="text-2xl font-bold mb-4">Invoice</h1>
 
-    <!-- Error state -->
+    <!-- Error State -->
     <div v-if="error" class="text-red-500 text-center mb-4">
       {{ error }}
     </div>
 
-    <!-- Loading state -->
+    <!-- Loading State -->
     <div v-if="loading" class="text-center text-lg">
       Loading data, please wait...
     </div>
@@ -51,11 +51,12 @@
               </div>
             </td>
             <td class="border px-4 py-2 text-center">
-              {{ transaction.borrow_date }}
+              {{ formatDate(transaction.borrow_date) }}
             </td>
             <td class="border px-4 py-2 text-center">
-              {{ transaction.return_date }}
+              {{ formatDate(transaction.return_date) }}
             </td>
+
             <td class="border px-4 py-2 text-center">
               {{ transaction.borrower_name }}
             </td>
@@ -64,7 +65,7 @@
             </td>
             <td class="border px-4 py-2 text-center">
               <button
-                @click="exportRowToPDF(transaction)"
+                @click="exportToPDF(transaction)"
                 class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
               >
                 Download PDF
@@ -75,31 +76,116 @@
       </table>
     </div>
 
-    <!-- Empty state -->
+    <!-- Empty State -->
     <div
       v-if="!loading && !Borrow.length"
       class="text-center text-gray-500 dark:text-gray-400"
     >
       Tidak ada data peminjaman yang tersedia.
     </div>
+
+    <!-- Hidden Invoice Template -->
+    <div id="invoice-template" v-if="selectedTransaction" class="hidden">
+      <div style="display: flex; align-items: center; margin: 1rem">
+        <img src="../assets/logo.png" alt="logo" class="w-52" />
+      </div>
+      <div class="text-center text-3xl font-bold">Borrowing Item</div>
+      <div class="text-center text-3xl font-bold mb-3">Invoice</div>
+      <div class="text-left text-md m-10">
+        <table class="text-left">
+          <tbody>
+            <tr>
+              <td class="py-2">Borrower Name:</td>
+              <td class="px-6 py-2">{{ selectedTransaction.borrower_name }}</td>
+            </tr>
+            <tr>
+              <td class="py-2">Operator:</td>
+              <td class="px-6 py-2">{{ selectedTransaction.officer_name }}</td>
+            </tr>
+            <tr>
+              <td class="py-2">Borrow Date:</td>
+              <td class="px-6 py-2">{{ selectedTransaction.borrow_date }}</td>
+            </tr>
+            <tr>
+              <td class="py-2">Return Date:</td>
+              <td class="px-6 py-2">{{ selectedTransaction.return_date }}</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table
+          class="table-auto w-full text-left border-collapse mt-8"
+          style="border: 1px solid black"
+        >
+          <thead style="border: 1px solid black">
+            <tr>
+              <th class="border px-4 py-2">Item Name</th>
+              <th class="border px-4 py-2">Amount</th>
+            </tr>
+          </thead>
+          <tbody style="border: 1px solid black">
+            <tr v-for="item in selectedTransaction.items" :key="item.item_id">
+              <td class="border px-4 py-2">{{ item.item_name }}</td>
+              <td class="border px-4 py-2">{{ item.amount }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="text-left text-md m-10">
+        <p>
+          Thank you for using our borrowing service. Below are the details of
+          the items you have borrowed. Please ensure to return the items by the
+          specified return date to avoid any late fees. If you have any
+          questions or need further assistance, feel free to contact our support
+          team.
+        </p>
+        <p>We hope you have a great experience with our service!</p>
+      </div>
+
+      <div class="text-end text-md mt-20 mr-6 mb-4">
+        <p>Signature</p>
+        <p>Admin</p>
+        <p>Lab KCKS</p>
+        <p>Surabaya, Indonesia</p>
+        <div class="flex justify-end mt-4">
+          <img src="../assets/signature.png" alt="signature" class="w-32" />
+        </div>
+        <p>(Agus Setiawan)</p>
+        <p>Invoice generated on {{ new Date().toLocaleDateString() }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import jsPDF from "jspdf";
-import "jspdf-autotable";
+import html2pdf from "html2pdf.js";
 
 export default {
   name: "SummaryPage",
   data() {
     return {
       Borrow: [],
+      selectedTransaction: null,
       error: "",
       loading: true,
     };
   },
   methods: {
+    formatDate(dateString) {
+      if (!dateString) return "N/A"; // Handle null or undefined dates
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        timeZoneName: "short",
+      };
+      return new Date(dateString).toLocaleDateString(undefined, options);
+    },
+
     async fetchData() {
       this.loading = true;
       try {
@@ -111,43 +197,42 @@ export default {
             },
           }
         );
-        if (response.data && response.data.data) {
-          this.Borrow = response.data.data;
-        } else {
-          this.error = "Data not in expected format.";
-        }
+        this.Borrow = response.data.data.map((transaction) => ({
+          ...transaction,
+          borrow_date: this.formatDate(transaction.borrow_date),
+          return_date: this.formatDate(transaction.return_date),
+        }));
       } catch (error) {
-        console.error("Error fetching data:", error);
-        this.error = "Error fetching data. Please try again later.";
+        this.error = "Failed to fetch data. Please try again later.";
       } finally {
         this.loading = false;
       }
     },
-    exportRowToPDF(transaction) {
-      const doc = new jsPDF();
-      doc.text("Invoice", 14, 10);
 
-      // Detail Informasi
-      doc.text(`Peminjam: ${transaction.borrower_name}`, 14, 20);
-      doc.text(`Teknisi: ${transaction.officer_name}`, 14, 30);
-      doc.text(`Tanggal Peminjaman: ${transaction.borrow_date}`, 14, 40);
-      doc.text(`Tanggal Pengembalian: ${transaction.return_date}`, 14, 50);
+    exportToPDF(transaction) {
+      this.selectedTransaction = transaction;
 
-      // Tabel Data Alat
-      const tableColumn = ["Nama Alat", "Jumlah"];
-      const tableRows = transaction.items.map((item) => [
-        item.item_name,
-        item.amount,
-      ]);
+      this.$nextTick(() => {
+        const element = document.getElementById("invoice-template");
+        element.classList.remove("hidden");
 
-      doc.autoTable({
-        startY: 60,
-        head: [tableColumn],
-        body: tableRows,
+        setTimeout(() => {
+          const options = {
+            margin: 1,
+            filename: `Invoice_${transaction.borrower_name}.pdf`,
+            html2canvas: { scale: 2 },
+            jsPDF: { orientation: "portrait" },
+          };
+
+          html2pdf()
+            .from(element)
+            .set(options)
+            .save()
+            .finally(() => {
+              element.classList.add("hidden");
+            });
+        }, 500);
       });
-
-      // Unduh PDF
-      doc.save(`Invoice_${transaction.borrower_name}.pdf`);
     },
   },
   mounted() {
@@ -157,16 +242,7 @@ export default {
 </script>
 
 <style scoped>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-th,
-td {
-  text-align: left;
-  padding: 8px;
-}
-th {
-  background-color: #f2f2f2;
+.hidden {
+  display: none;
 }
 </style>
